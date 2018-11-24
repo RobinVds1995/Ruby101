6a06e681d14051d3a44835b95a30c23734c2d8a9
RobinVds1995 <robin.vanderschueren@ae.be>
2018-11-24 11:26:51 +0100

    tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_172142) do
+ActiveRecord::Schema.define(version: 2018_11_24_084713) do
 
   create_table "articlas", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,6 +31,21 @@ ActiveRecord::Schema.define(version: 2018_11_23_172142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
