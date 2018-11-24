2824f2ff84e75ccdcc58ad24bd9aa8a0bfe72050
RobinVds1995 <robin.vanderschueren@ae.be>
2018-11-24 09:40:55 +0100

    Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_155735) do
+ActiveRecord::Schema.define(version: 2018_11_23_172142) do
 
   create_table "articlas", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2018_11_22_155735) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
